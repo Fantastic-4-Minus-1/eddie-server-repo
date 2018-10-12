11558824d9dd3d555d302044745fe1fddbc3c3f6
EddieLo <eddielo106@gmail.com>
2018-10-11 19:12:53 -0700

    fixed cache save after retrieving from database

--- server/redis.js
+++ server/redis.js
@@ -3,9 +3,11 @@ require('newrelic');
 const express = require('express');
 const parser = require('body-parser');
 const compression = require('compression');
+const path = require('path');
+const axios = require('axios');
 const proxy = require('express-http-proxy');
 
-const { cache } = require('../database/redis');
+const { save, cache } = require('../database/redis');
 const app = express();
 
 app.use(compression());
@@ -15,7 +17,18 @@ app.set('PORT', process.env.PORT || 3000);
 
 app.use('/:companyAbbr', express.static('public'));
 
-app.use('/', cache , proxy('http://ec2-54-173-182-247.compute-1.amazonaws.com'));
+const url = 'http://ec2-54-173-182-247.compute-1.amazonaws.com';
+
+app.get('/api/people-also-bought/:abbrOrId', cache, (req, res, next) => {
+  axios.get(url + req.url)
+    .then(({ data }) => {
+      save(path.basename(req.url), JSON.stringify(data))
+      res.send(data)
+    })
+    .catch(error => res.status('400').send(error));
+});
+
+app.use('/', (proxy(url)));
 
 app.listen(app.get('PORT'), () => {
   console.log(`Server is connected to ${app.get('PORT')}!`);
