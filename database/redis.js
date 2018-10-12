695c4b3f4f35dc51d81afe52cfc44ad5f2cc0deb
EddieLo <eddielo106@gmail.com>
2018-10-11 19:57:53 -0700

    clean-up

--- database/redis.js
+++ database/redis.js
@@ -5,24 +5,20 @@ const model = require('../server/model');
 
 const client = redis.createClient(process.env.REDIS_PORT || 6379);
 
-const save = (key, value) => {
-  return client.setexAsync(key, 5, value);
-};
+const save = (key, value) => client.setexAsync(key, 5, value);
 
-const cache = (req, res, next) => {
-  return client.getAsync(path.basename(req.url))
-    .then(data => {
-      if (data !== null && req.method === 'GET') {
-        res.send(data);
-        return model.peopleAlsoBought.get(path.basename(req.url))
-          .then(data => save(path.basename(req.url), JSON.stringify(data)))
-          .catch(err => console.log(err.stack));
-      } else {
-        next();
-      }
-    })
-    .catch(error => res.status('401').send(error));
-};
+const cache = (req, res, next) => client.getAsync(path.basename(req.url))
+  .then((data) => {
+    if (data !== null && req.method === 'GET') {
+      res.send(data);
+      model.peopleAlsoBought.get(path.basename(req.url))
+        .then(response => save(path.basename(req.url), JSON.stringify(response)))
+        .catch(error => console.log(error.stack));
+    } else {
+      next();
+    }
+  })
+  .catch(error => res.status('401').send(error));
 
 module.exports = {
   save,
